90a2d528b7ee3fb4ccd77010fbcfb82eba8a09e6
chaim <chchdeu@gmail.com>
2026-06-24 19:20:56 +0300

    mission_2

--- explorer.py
+++ explorer.py
@@ -1,4 +1,7 @@
 #mission 1
-fruits=["appale","banana","cherry"]
-for fruit in fruits:
-    print(fruit)
+# fruits=["appale","banana","cherry"]
+# for fruit in fruits:
+#     print(fruit)
+#misson 2
+i=range(1,6)
+print(list(i))
