99f484c49d727cf6c8e944e1b36878784484970a
chaim <chchdeu@gmail.com>
2026-06-24 19:27:28 +0300

    mission_4

--- explorer.py
+++ explorer.py
@@ -1,9 +1,12 @@
 #mission 1
-# fruits=["appale","banana","cherry"]
+fruits=["appale","banana","cherry"]
 # for fruit in fruits:
 #     print(fruit)
 #misson 2
 # i=range(1,6)
 # print(list(i))
 #mission 3
-print(list(range(0,10,2)))
+# print(list(range(0,10,2)))
+#nission 4
+for fruit in enumerate(fruits):
+    print(fruit)
