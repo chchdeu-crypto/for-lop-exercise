e9da2cf7c1ef269763f45fe2858e62f69a739609
chaim <chchdeu@gmail.com>
2026-06-24 19:45:33 +0300

    mission_7

--- explorer.py
+++ explorer.py
@@ -15,8 +15,15 @@ fruits=["appale","banana","cherry"]
 # for score in scores.items():
 #     print(score)
 #mission 6
-numbers=[1,2,3,4,5,]
-sum=0
-for num in numbers:
-    sum+=num
-print(sum)  
+# numbers=[1,2,3,4,5,]
+# sum=0
+# for num in numbers:
+#     sum+=num
+# print(sum)
+# misson 7
+i=0
+while i < 5:
+    i+=1
+    print(i)
+for i in range(1,6):
+    print(i)
